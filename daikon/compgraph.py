227a03a37651e43bc322f898e6416c97b7fbf6f1
rborrello <38610433+rborrello@users.noreply.github.com>
2019-05-11 10:44:28 +0200

    Dropout implementation

--- daikon/compgraph.py
+++ daikon/compgraph.py
@@ -40,9 +40,10 @@ def define_computation_graph(source_vocab_size: int, target_vocab_size: int, bat
 
     with tf.variable_scope("Encoder"):
         encoder_cell = tf.contrib.rnn.LSTMCell(C.HIDDEN_SIZE)
-        initial_state = encoder_cell.zero_state(batch_size, tf.float32)
+        dropout_cell = tf.nn.dropout((encoder_cell, 0.5), dtype=tf.float32)  ## rborre: Dropout implementation with rate XXX of 0.5
+        initial_state = dropout_cell.zero_state(batch_size, tf.float32)
 
-        encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(encoder_cell,
+        encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(dropout_cell,                  ## rborre: Consequence of the Dropout implememtation
                                                                  encoder_inputs_embedded,
                                                                  initial_state=initial_state,
                                                                  dtype=tf.float32)
